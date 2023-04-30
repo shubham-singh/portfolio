82d301487c7d69b389256c1b68557c487b1fe000
Shubham Singh <shubhamsng7@gmail.com>
2023-04-30 11:59:25 +0530

    read stocks data from json file

--- index.js
+++ index.js
@@ -1,89 +1,73 @@
-// const stocks = [
-//     {
-//         symbol: "ITC",
-//         currency: "₹",
-//         averagePrice: "328.58",
-//         quantity: 418
-//     },
-//     {
-//         symbol: "JCHAC",
-//         currency: "₹",
-//         averagePrice: "1184.62",
-//         quantity: 65
-//     },
-//     {
-//         symbol: "JSWENERGY",
-//         currency: "₹",
-//         averagePrice: "268.08",
-//         quantity: 50
-//     },
-//     {
-//         symbol: "KOTAKALPHA",
-//         currency: "₹",
-//         averagePrice: "29.24",
-//         quantity: 2700
-//     },
-//     {
-//         symbol: "KOTAKNV20",
-//         currency: "₹",
-//         averagePrice: "99.77",
-//         quantity: 781
-//     },
-//     {
-//         symbol: "MAXHEALTH",
-//         currency: "₹",
-//         averagePrice: "261.00",
-//         quantity: 1
-//     },
-//     {
-//         symbol: "SBIETFIT",
-//         currency: "₹",
-//         averagePrice: "264.49",
-//         quantity: 5
-//     }
-// ]
-
-// const totalInvestedValue = Number(stocks.reduce((total, stock) => {
-//     total += Number(stock.averagePrice) * Number(stock.quantity)
-//     return total
-// }, 0).toFixed(2))
-
-// stocks.forEach((stock) => {
-//     const stockValue = Number(stock.averagePrice) * Number(stock.quantity)
-//     const investedWeightage = Number((stockValue / totalInvestedValue) * 100).toFixed(2)
-//     stock['investedWeightage'] = investedWeightage
-// })
-
-
-async function getStocks () {        
-    const response = await fetch('./stocks.json')
-    console.log(response)
-    const data = await response.json()
-    console.log('data', data)
-    // setStocks(data)
+async function getStockPrice(symbol) {
+    try {
+        const apiKey = 'dummyAPIKey';
+        const response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}.BSE&apikey=${apiKey}`)
+        const data = await response.json()
+        const currentPrice = data['Global Quote']['05. price']
+        return currentPrice ? Number(currentPrice).toFixed(2) : undefined
+    } catch (error) {
+        console.error(error)
+    }
 }
-
-getStocks()
 
 function App() {
     const [stocks, setStocks] = React.useState([]);
-    // console.log('Hiii')
-    // console.log(stocks)
-    React.useEffect(function () {}, [])
+
+    React.useEffect(async function () {
+        try {
+            const response = await fetch('./stocks.json')
+            const data = await response.json()
+            const promises = data.map((stock) => getStockPrice(stock.symbol))
+            const currentPrices = await Promise.all(promises)
+            data.forEach((stock, index) => {
+                stock.currentPrice = currentPrices[index]
+            })
+            setStocks(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }, [])
+
+    const totalInvestedValue = Number(stocks.reduce((total, stock) => {
+        total += Number(stock.averagePrice) * Number(stock.quantity)
+        return total
+    }, 0).toFixed(2))
+
+    const totalPresentValue = Number(stocks.reduce((total, stock) => {
+        total += Number(stock.currentPrice ?? stock.averagePrice) * Number(stock.quantity)
+        return total
+    }, 0).toFixed(2))
+
+    let showCurrentWeightage = false
+
+    stocks.forEach((stock) => {
+        const investedStockValue = Number(stock.averagePrice) * Number(stock.quantity)
+        const investedWeightagePercentage = Number((investedStockValue / totalInvestedValue) * 100).toFixed(2)
+        stock['investedWeightage'] = investedWeightagePercentage
+        if (stock.currentPrice !== undefined) {
+            const currentWeightage = Number(stock.currentPrice) * Number(stock.quantity)
+            const currentWeightagePercentage = Number((currentWeightage / totalPresentValue) * 100).toFixed(2)
+            stock['currentWeightage'] = currentWeightagePercentage
+            showCurrentWeightage = true
+        } else {
+            showCurrentWeightage = false
+        }
+    })
+
     return <>
         <table>
             <tr>
                 <th>Symbol</th>
                 <th>Average Price</th>
                 <th>Weightage (Invested)</th>
-                <th>Weightage (Present)</th>
+                {showCurrentWeightage && <th>Weightage (Present)</th>}
             </tr>
             {stocks.map((stock) => {
                 return <tr>
                     <td>{stock.symbol}</td>
                     <td>{stock.currency}{stock.averagePrice}</td>
                     <td>{stock.investedWeightage}%</td>
-                    <td>coming soon</td>
+                    {showCurrentWeightage && <td>{stock.currentWeightage}%</td>}
                 </tr>
             })}
         </table>
